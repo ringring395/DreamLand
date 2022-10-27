54fc87ba862756f8feb6517af77c97eb4db23481
ringring <ringring395@gmail.com>
2022-10-27 11:47:47 +0900

    10/27 main : 메인 공지사항 출력 완료

--- Ringring/src/main/java/com/ring/controller/HomeController.java
+++ Ringring/src/main/java/com/ring/controller/HomeController.java
@@ -1,15 +1,25 @@
 package com.ring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.ring.model.BoardVO;
+import com.ring.service.BoardService;
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	BoardService bs;
+	
 	//메인페이지
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String main() {
+	public String main(BoardVO board, Model model) {
+		
+		model.addAttribute("mainNotice", bs.mainNotice(board));
 		
 		return "/Home/main";
 	}
